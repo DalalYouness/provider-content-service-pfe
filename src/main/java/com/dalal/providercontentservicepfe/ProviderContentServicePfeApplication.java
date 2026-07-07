831a50452e63e6e4021b916619cd89fca2bbf27a
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-07-07 12:17:50 +0100

    project setup

---
+++ src/main/java/com/dalal/providercontentservicepfe/ProviderContentServicePfeApplication.java
@@ -0,0 +1,13 @@
+package com.dalal.providercontentservicepfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderContentServicePfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderContentServicePfeApplication.class, args);
+    }
+
+}
